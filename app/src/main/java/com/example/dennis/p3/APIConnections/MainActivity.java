b1c930ca56b151dba0879d36027b7a5fc1991635
Dennis Kristensson <Dennis@Denniss-MacBook-Pro.local>
2016-10-27 13:10:51 +0200

    done

--- app/src/main/java/com/example/dennis/p3/APIConnections/MainActivity.java
+++ app/src/main/java/com/example/dennis/p3/APIConnections/MainActivity.java
@@ -107,7 +107,7 @@ public class MainActivity extends Activity implements PlayerNotificationCallback
 
     @Override
     public void onBackPressed() {
-        finish();
+        System.exit(0);
     }
 
     @Override
